5281f08f374d4158ce62a7a3fb5ef6634d030a73
Chris Muga <chrismuga94@gmail.com>
2019-03-21 12:31:53 +0300

    changes on install page

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
                 <a class="nav-link" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="install.php">Install</a>
+                <a class="nav-link" href="install.php">How it Works</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="our-products.php">Our Products</a>
